231d3e6d2ddd9c48af739d2a28c1dfd4f1a691fc
martinmartinchan <martinchan994@gmail.com>
2021-03-08 14:24:45 +0100

    feat(rollup-plugin-curry): printer that generates curry combinations

--- modules/rollup-plugin-curry/lib/curry-definition.js
+++ modules/rollup-plugin-curry/lib/curry-definition.js
@@ -1,9 +1,7 @@
 import parse from './parse.js'
-import printers from './printers.js'
+import curryPrint from './curry-print.js'
 
 export default function cd (functionDeclaration) {
   const definition = parse(functionDeclaration)
-  const { parameters } = definition
-  const print = printers[parameters.length - 1]
-  return print(definition)
+  return curryPrint(definition)
 }
