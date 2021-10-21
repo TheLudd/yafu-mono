b38817ece8293ee1fd41d66c015f99296f50ff9d
Ludwig Magnusson <ludwig@mediatool.com>
2021-10-21 10:51:35 +0200

    fix(transduce): curry into

--- modules/transduce/lib/into.js
+++ modules/transduce/lib/into.js
@@ -1,6 +1,9 @@
+import { curry } from 'yafu'
 import { init } from './transformers/utils.js'
 import runTransduce from './run-transduce.js'
 
-export default function into (transformer, transducer, iterable) {
+function into (transformer, transducer, iterable) {
   return runTransduce(transducer, transformer, init(transformer), iterable)
 }
+
+export default curry(into)
