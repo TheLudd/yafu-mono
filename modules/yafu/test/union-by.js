102e2c4498df05ca689d852fa8fba46a09355754
Tautvydas Gecevicius <gecevicius.tautvydas@gmail.com>
2021-11-01 11:20:55 +0100

    feat(yafu): add unionBy

---
+++ modules/yafu/test/union-by.js
@@ -0,0 +1,33 @@
+import I from '../lib/i.js'
+
+export default function (unionBy) {
+  return function () {
+    it('it returns union of 2 arrays', () => {
+      unionBy(I, [ 'a', 'b', 'c' ], [ 'c', 'd', 'e' ]).should.deep.equal([ 'a', 'b', 'c', 'd', 'e' ])
+    })
+
+    it('handles applied func', () => {
+      unionBy((a) => a.toUpperCase(), [ 'a', 'b', 'c' ], [ 'C', 'd', 'e' ]).should.deep.equal([ 'a', 'b', 'c', 'd', 'e' ])
+    })
+
+    it('handles duplicate in first array', () => {
+      unionBy(I, [ 'a', 'b', 'b', 'c' ], [ 'c', 'd', 'e' ]).should.deep.equal([ 'a', 'b', 'c', 'd', 'e' ])
+    })
+
+    it('handles duplicates in second array', () => {
+      unionBy(I, [ 'a', 'b', 'c' ], [ 'c', 'd', 'd', 'e' ]).should.deep.equal([ 'a', 'b', 'c', 'd', 'e' ])
+    })
+
+    it('handles both empty arrays', () => {
+      unionBy(I, [], []).should.deep.equal([])
+    })
+
+    it('handles first empty array', () => {
+      unionBy(I, [], [ 'c', 'd', 'e' ]).should.deep.equal([ 'c', 'd', 'e' ])
+    })
+
+    it('handles second empty array', () => {
+      unionBy(I, [ 'a', 'b', 'c' ], []).should.deep.equal([ 'a', 'b', 'c' ])
+    })
+  }
+}
