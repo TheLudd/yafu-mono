82c98d9ebf6f682b2788c734817efd74e90dc6bc
Ludwig Magnusson <ludwig@mediatool.com>
2022-08-02 14:52:37 +0200

    chore(either): transpile with esbuild

--- modules/either/rollup.config.js
+++
@@ -1,4 +0,0 @@
-import { createRollupConfig } from '@yafu/create-rollup-config'
-import pkg from './package.json'
-
-export default createRollupConfig('./lib/either.ts', pkg)
---
+++ modules/either/rollup.config.mjs
@@ -0,0 +1,3 @@
+import { createRollupConfig } from '@yafu/create-rollup-config'
+
+export default createRollupConfig('./lib/either.ts', import.meta.url)
