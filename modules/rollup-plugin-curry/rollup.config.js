8edca192cf02cb1547a5b6287484e7593bac587f
Ludwig Magnusson <ludwig@mediatool.com>
2023-01-01 18:51:03 +0100

    fix: add type exports to all eligable modules

--- modules/rollup-plugin-curry/rollup.config.js
+++ modules/rollup-plugin-curry/rollup.config.js
@@ -1,10 +1,11 @@
 import esbuild from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
 
-export default {
+export default [ {
   input: './lib/plugin.ts',
   plugins: [ esbuild() ],
   output: [ {
-    file: 'dist/es/plugin.mjs',
+    file: 'dist/es/plugin.js',
     format: 'es',
     exports: 'default',
     sourcemap: true,
@@ -14,4 +15,11 @@ export default {
     exports: 'default',
     sourcemap: true,
   } ],
-}
+}, {
+  input: './lib/plugin.ts',
+  plugins: [ dts() ],
+  output: {
+    file: 'dist/types.d.ts',
+    format: 'es',
+  },
+} ]
--- modules/type-utils/index.d.ts
+++ modules/type-utils/index.d.ts
@@ -1,2 +1,2 @@
-export * from './lib/hkt'
-export * from './lib/functions'
+export * from './lib/hkt.js'
+export * from './lib/functions.js'
