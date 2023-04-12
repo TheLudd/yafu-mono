66e130155febabe10f5ff509966e30835373f741
Ludwig Magnusson <ludwig@mediatool.com>
2023-04-12 21:59:51 +0200

    chore(sort-dependencies): remove transpilation
    Since all modules are in es6 now, there is no need to transpile this
    internal tool to commonjs.

--- modules/sort-dependencies/rollup.config.js
+++
@@ -1,4 +0,0 @@
-import resolve from '@rollup/plugin-node-resolve'
-import { createRollupConfig } from '@yafu/create-rollup-config'
-
-export default createRollupConfig('./lib/sort-dependencies.js', import.meta.url, resolve())
