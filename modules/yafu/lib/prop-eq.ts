5a8ef7cd08ba2a8706c41f91e77ca60c935b5369
martinmartinchan <martinchan994@gmail.com>
2023-06-02 13:35:04 +0200

    fix(yafu): type with any instead of unknown

--- modules/yafu/lib/prop-eq.ts
+++ modules/yafu/lib/prop-eq.ts
@@ -8,7 +8,8 @@
 export function propEq(
   name: string,
   val: unknown,
-  o: Record<string, unknown>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  o: Record<string, any>,
 ): boolean {
   return o[name] === val
 }
