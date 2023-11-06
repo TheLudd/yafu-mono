1feb8518f69e5422ae57c0858892c6c781ab4b67
Ludwig Magnusson <ludwig@mediatool.com>
2023-11-06 10:51:05 +0100

    fix(maybe): add of to the extensions of fantasy-functions

--- modules/maybe/lib/extensions.d.ts
+++ modules/maybe/lib/extensions.d.ts
@@ -5,5 +5,6 @@ declare module '@yafu/fantasy-functions' {
   export function ap<T, U>(a: Maybe<Unary<T, U>>, apply: Maybe<T>): Maybe<U>
   export function chain<T, U>(f: Unary<T, Maybe<U>>, m: Maybe<T>): Maybe<U>
   export function map<T, U>(f: Unary<T, U>, m: Maybe<T>): Maybe<U>
+  export function of<T>(m: typeof Maybe, value: T): Maybe<T>
   export function reduce<T, U>(f: Fold<T, U>, seed: U, m: Maybe<T>): U
 }
--- modules/maybe/test/maybe.ts
+++ modules/maybe/test/maybe.ts
@@ -1,10 +1,9 @@
 import { assert } from 'chai'
-import { ap, chain, equals, map, reduce, of } from '@yafu/fantasy-functions'
-import { Maybe, nothing } from '../lib/maybe.js'
+import { ap, chain, equals, map, reduce } from '@yafu/fantasy-functions'
+import { maybeOf, nothing } from '../lib/maybe.js'
 
 const inc = (a: number) => a + 1
 
-const maybeOf = of(Maybe)
 const m1 = maybeOf(1)
 
 const { isTrue, isFalse, deepEqual, equal } = assert
