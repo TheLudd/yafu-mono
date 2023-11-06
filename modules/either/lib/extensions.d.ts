82b8ef9b0c1a462b78239c156c854577bdadef4b
Ludwig Magnusson <ludwig@mediatool.com>
2023-11-06 11:04:22 +0100

    fix(either): add of to the extensions of fantasy-functions

--- modules/either/lib/extensions.d.ts
+++ modules/either/lib/extensions.d.ts
@@ -5,5 +5,6 @@ declare module '@yafu/fantasy-functions' {
   export function ap<T, U, L>(f: Either<L, Unary<T, U>>, either: Either<L, T>): Either<L, U>
   export function chain<T, U, L>(f: Unary<T, Either<L, U>>, either: Either<L, T>): Either<L, U>
   export function map<T, U, L>(f: Unary<T, U>, either: Either<L, T>): Either<L, U>
+  export function of<T>(e: typeof Either, v: T): Either<never, T>
   export function reduce<T, U, L>(f: Fold<T, U>, seed: U, either: Either<L, T>): U
 }
